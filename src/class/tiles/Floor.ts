e8cec363d531d81723b61a47531035363568ba0f
Tenebrosful <hugobernard39@gmail.com>
2021-12-15 03:01:34 +0100

    Create Floor.ts

---
+++ src/class/tiles/Floor.ts
@@ -0,0 +1,11 @@
+import { tileType } from "../../enum/tileType";
+import { Tile } from "./Tile";
+
+export class Floor extends Tile{
+  type = tileType.FLOOR;
+
+  constructor(params: {posX: number, posY: number}) {
+    super({...params, canFlyOver: true, canWalkThrough: true});
+  }
+  
+}
