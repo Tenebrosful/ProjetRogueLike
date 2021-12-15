5159304124b74a5a6f9bd8d9556d54b82474c64b
Tenebrosful <hugobernard39@gmail.com>
2021-12-15 03:01:30 +0100

    Create Wall.ts

---
+++ src/class/tiles/Wall.ts
@@ -0,0 +1,11 @@
+import { tileType } from "../../enum/tileType";
+import { Tile } from "./Tile";
+
+export class Wall extends Tile{
+  type = tileType.WALL;
+
+  constructor(params: {posX: number, posY: number}) {
+    super({...params, canFlyOver: false, canWalkThrough: false});
+  }
+  
+}
