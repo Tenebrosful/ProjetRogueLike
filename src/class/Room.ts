a147760173e6016b397e0bc71c9f84123d53b799
Tenebrosful <hugobernard39@gmail.com>
2021-12-15 04:20:26 +0100

    Add a function that generate a random room

--- src/class/Room.ts
+++ src/class/Room.ts
@@ -1,6 +1,8 @@
+import { RandomSeed } from "random-seed";
 import { tileType } from "../enum/tileType";
 import { tileResolver } from "../resolver/TileResolver";
 import { Tile } from "./tiles/Tile";
+import * as rooms from "../../config/room.json";
 
 export class Room {
   tiles: Tile[][];
@@ -15,6 +17,10 @@ export class Room {
         /* @ts-ignore */
         this.tiles[posY].push(new (tileResolver(tiles[posY][posX]))({ posX, posY }));
     }
+  }
+
+  static generateRandom(random: RandomSeed) {
+    return new Room(rooms[random.intBetween(0, rooms.length - 1)] as tileType[][]);
   }
 
   renderTextTiles() {
