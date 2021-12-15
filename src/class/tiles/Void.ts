eda43644688839eabedeb204ccad13327a7fc56a
Tenebrosful <hugobernard39@gmail.com>
2021-12-15 03:01:32 +0100

    Create Void.ts

---
+++ src/class/tiles/Void.ts
@@ -0,0 +1,11 @@
+import { tileType } from "../../enum/tileType";
+import { Tile } from "./Tile";
+
+export class Void extends Tile{
+  type = tileType.VOID;
+
+  constructor(params: {posX: number, posY: number}) {
+    super({...params, canFlyOver: true, canWalkThrough: false});
+  }
+  
+}
