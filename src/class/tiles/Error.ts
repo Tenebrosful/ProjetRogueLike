e5a2d833c781ef57a0db4c0dd5d53c0544c843de
Tenebrosful <hugobernard39@gmail.com>
2021-12-15 03:01:37 +0100

    Create Error.ts

---
+++ src/class/tiles/Error.ts
@@ -0,0 +1,11 @@
+import { tileType } from "../../enum/tileType";
+import { Tile } from "./Tile";
+
+export class Error extends Tile{
+  type = tileType.ERROR;
+
+  constructor(params: {posX: number, posY: number}) {
+    super({...params, canFlyOver: true, canWalkThrough: true});
+  }
+  
+}
