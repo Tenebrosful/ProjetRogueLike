8b0271987684a498f00cfa8134a6515169f77c4f
Clément Boulet <clement.boulet60510@gmail.com>
2021-12-16 10:52:17 +0100

    Debut de mongodb

--- serveur.js
+++ serveur.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/user-db',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 const app = express()
 app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
 
+app.post('/api/register', async(req,res)=>{
+    console.log(req.body)
+    res.json({ status: 'ok' })
+})
 
 app.listen(9999, () => {
     console.log('Serveur up at 9999')
