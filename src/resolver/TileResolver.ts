d5af46643248f9b65a558818c42d65d4987c3d0d
Tenebrosful <hugobernard39@gmail.com>
2021-12-16 15:17:12 +0100

    Refactor into a different function

--- src/resolver/TileResolver.ts
+++ src/resolver/TileResolver.ts
@@ -1,27 +1,23 @@
 import { tileType } from "../enum/tileType";
-import { TileFactory } from "../class/tiles/factory/TileFactory";
 import { FloorFactory } from "../class/tiles/factory/FloorFactory";
 import { ErrorFactory } from "../class/tiles/factory/ErrorFactory";
 import { VoidFactory } from "../class/tiles/factory/VoidFactory";
 import { WallFactory } from "../class/tiles/factory/WallFactory";
 import { Tile } from "../class/tiles/Tile";
 
-export function tileResolver(type: tileType, coords: {posX: number, posY: number}): Tile {
-  let factory: TileFactory;
+export function tileResolver(type: tileType, coords: { posX: number, posY: number }): Tile {
+  return getTileFactory(type).createTile(coords);
+}
+
+function getTileFactory(type: tileType) {
   switch (type) {
     case tileType.FLOOR:
-      factory = new FloorFactory;
-      break;
+      return new FloorFactory;
     case tileType.WALL:
-      factory = new WallFactory;
-      break;
+      return new WallFactory;
     case tileType.VOID:
-      factory = new VoidFactory;
-      break;
+      return new VoidFactory;
     default:
-      factory = new ErrorFactory;
-      break;
+      return new ErrorFactory;
   }
-
-  return factory.createTile(coords);
 }
