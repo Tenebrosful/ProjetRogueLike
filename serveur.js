cb5f4fbae00d653963c87ecb83372cd304947462
Clément Boulet <clement.boulet60510@gmail.com>
2021-12-16 13:32:52 +0100

    Enregistrement en bdd

--- serveur.js
+++ serveur.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const User = require('./src/model/user.js')
+const bcrypt = require('bcryptjs')
 
 mongoose.connect('mongodb://localhost:27017/user-db',{
     useNewUrlParser: true,
@@ -14,6 +16,19 @@ app.use(bodyParser.json())
 
 app.post('/api/register', async(req,res)=>{
     console.log(req.body)
+    const { username, password : plainTextPassword } = req.body;
+    const password = await bcrypt.hash(plainTextPassword,10)
+    try{
+        const response = await User.create({
+            username,
+            password
+        })
+        console.log('User created: ',response)
+    }catch(error){
+        console.log(error)
+        return res.json({status: 'error'})
+    }
+    console.log(password)
     res.json({ status: 'ok' })
 })
 
