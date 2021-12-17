fdb309062a32423f446902bb586cfb0b15bf5acd
Tenebrosful <hugobernard39@gmail.com>
2021-12-17 22:43:03 +0100

    Change to .ts with import syntax

--- src/serv-web/models/User.ts
+++ src/serv-web/models/User.ts
@@ -1,10 +1,8 @@
-const mongoose = require('mongoose')
+import * as mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true}
 }, { collection: 'users'})
 
-const model = mongoose.model('UserSchema', UserSchema)
-
-module.exports = model
+export const User = mongoose.model('UserSchema', UserSchema);
---
+++ src/serv-web/serveur.ts
@@ -0,0 +1,112 @@
+import * as express from "express";
+import * as path from "path";
+import * as bodyParser from "body-parser";
+import * as mongoose from "mongoose";
+import { User } from "./models/User";
+import * as bcrypt from "bcryptjs";
+import * as jwt from "jsonwebtoken";
+
+const JWT_SECRET = "La réussite est une fleur qu'on arrose de volonté - Lucas Da Silva";
+
+mongoose.connect('mongodb://localhost:27017/user-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.listen(9999, () => {
+    console.log('Serveur up at 9999')
+})
+app.use(bodyParser.json())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.post('/api/signin', async (req, res) => {
+
+    const { username, password } = req.body
+    const user = await User.findOne({ username }).lean()
+
+    if (!user) {
+        return res.json({ status: 'error', error: 'Invalid username or password' })
+    }
+
+    if (await bcrypt.compare(password, user.password)) {
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+                username: user.username
+            },
+            JWT_SECRET
+        )
+        return res.json({ status: 'ok', data: token })
+    }
+
+    res.json({ status: 'error', error: 'Invalid username or password' })
+})
+
+app.post('/api/signup', async (req, res) => {
+
+    const { username, password: plainTextPassword } = req.body;
+
+    if (!username || typeof username !== 'string') {
+        return res.json({ status: 'error', error: 'Invalid username' })
+    }
+
+    if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+        return res.json({ status: 'error', error: 'Invalid password' })
+    }
+
+    //On peut aussi faire des restrictions demandant une majuscule
+    if (plainTextPassword.length < 5) {
+        return res.json({ status: 'error', error: 'Password too small. Should be atleast 6 characters' })
+    }
+
+    const password = await bcrypt.hash(plainTextPassword, 10)
+    try {
+        const response = await User.create({
+            username,
+            password
+        })
+        console.log('User created: ', response)
+    } catch (error) {
+        if (error.code === 11000) {
+            //duplicate key
+            return res.json({ status: 'error', error: 'Username already in use' })
+        }
+        throw error
+    }
+    console.log(password)
+    res.json({ status: 'ok' })
+})
+
+app.post('/api/change-password', async (req, res) => {
+    const { token, newpassword: plainTextPassword } = req.body
+
+    if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+        return res.json({ status: 'error', error: 'Invalid password' })
+    }
+
+    if (plainTextPassword.length < 5) {
+        return res.json({ status: 'error', error: 'Password too small. Should be atleast 6 characters' })
+    }
+
+
+    try {
+        const user = jwt.verify(token, JWT_SECRET)
+        const _id = user.id
+        const password = await bcrypt.hash(plainTextPassword, 10)
+        await User.updateOne({ _id }, {
+            $set: { password }
+        })
+        res.json({ status: 'ok' })
+    } catch (error) {
+        res.json({ status: 'error', error: ' :) ' })
+    }
+})
+
+const userRouter = require('./routes/users')
+app.use('/', userRouter)
