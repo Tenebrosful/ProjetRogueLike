4b7573636ef485b3fb0199eb97c6580f6309aa32
Tenebrosful <hugobernard39@gmail.com>
2021-12-17 14:59:22 +0100

    Adding typing function

--- src/class/tiles/Tile.ts
+++ src/class/tiles/Tile.ts
@@ -1,5 +1,9 @@
 import { tileType } from "../../enum/tileType";
 import { Coordinates, TileProperties } from "../../../typing/tiles";
+import { Floor } from "./Floor";
+import { Void } from "./Void";
+import { Wall } from "./Wall";
+import { Error } from "./Error";
 
 export abstract class Tile {
   posX: number;
@@ -11,6 +15,22 @@ export abstract class Tile {
 
   constructor({ posX, posY, canWalkThrough = true, canFlyOver = true, textRender = "?" }: Coordinates & TileProperties) {
     this.posX = posX; this.posY = posY; this.canWalkThrough = canWalkThrough; this.canFlyOver = canFlyOver; this.textRender = textRender;
+  }
+
+  isFloor(): this is Floor {
+    return this.type === tileType.FLOOR;
+  }
+
+  isVoid(): this is Void {
+    return this.type === tileType.VOID;
+  }
+
+  isWall(): this is Wall {
+    return this.type === tileType.WALL;
+  }
+
+  isError(): this is Error {
+    return this.type === tileType.ERROR || !(this.type in tileType);
   }
 
 }
