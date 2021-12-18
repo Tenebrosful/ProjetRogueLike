d240a67d434f71c8172592d7c076a45d9bb4a3d1
Tenebrosful <hugobernard39@gmail.com>
2021-12-18 04:49:35 +0100

    Add status to responses

--- src/serv-web/routes/account.ts
+++ src/serv-web/routes/account.ts
@@ -13,86 +13,71 @@ const JWT_SECRET = process.env.JWT_SECRET || randomBytes(10).toString("hex");
 
 account.post('/signin', async (req, res) => {
 
-  const { username, password } = req.body
-  const user = await User.findOne({ username }).lean()
+    const { username, password } = req.body
+    const user = await User.findOne({ username }).lean()
 
-  if (!user) {
-      return res.json({ status: 'error', error: 'Invalid username or password' })
-  }
+    if (!user) { res.status(400).json({ status: 'error', error: 'Invalid username or password' }); return; }
 
-  if (await bcrypt.compare(password, user.password)) {
+    if (!await bcrypt.compare(password, user.password)) { res.status(400).json({ status: 'error', error: 'Invalid username or password' }); return; }
 
-      const token = jwt.sign(
-          {
-              id: user._id,
-              username: user.username
-          },
-          JWT_SECRET
-      )
-      return res.json({ status: 'ok', data: token })
-  }
+    const token = jwt.sign(
+        {
+            id: user._id,
+            username: user.username
+        },
+        JWT_SECRET
+    )
 
-  res.json({ status: 'error', error: 'Invalid username or password' })
+    res.status(200).json({ status: 'ok', data: token });
+
 });
 
 account.post('/signup', async (req, res) => {
 
-  const { username, password: plainTextPassword } = req.body;
+    const { username, password: plainTextPassword } = req.body;
 
-  if (!username || typeof username !== 'string') {
-      return res.json({ status: 'error', error: 'Invalid username' })
-  }
+    if (!username || typeof username !== 'string') { res.status(400).json({ status: 'error', error: 'Invalid username' }); return; }
 
-  if (!plainTextPassword || typeof plainTextPassword !== 'string') {
-      return res.json({ status: 'error', error: 'Invalid password' })
-  }
+    if (!plainTextPassword || typeof plainTextPassword !== 'string') { res.status(400).json({ status: 'error', error: 'Invalid password' }); return; }
 
-  //On peut aussi faire des restrictions demandant une majuscule
-  if (plainTextPassword.length <= 5) {
-      return res.json({ status: 'error', error: 'Password too small. Should be atleast 6 characters' })
-  }
+    //On peut aussi faire des restrictions demandant une majuscule
+    if (plainTextPassword.length <= 5) { res.status(400).json({ status: 'error', error: 'Password too small. Should be atleast 6 characters' }); return; }
 
-  const password = await bcrypt.hash(plainTextPassword, 10)
-  try {
-      const response = await User.create({
-          username,
-          password
-      })
-      console.log('User created: ', response)
-  } catch (error) {
-      if (error instanceof Error && error.name === "MongoError" && (error as MongoError).code === 11000) {
-          //duplicate key
-          return res.json({ status: 'error', error: 'Username already in use' })
-      }
-      throw error
-  }
-  console.log(password)
-  res.json({ status: 'ok' })
+    const password = await bcrypt.hash(plainTextPassword, 10)
+    try {
+        const response = await User.create({
+            password,
+            username
+        })
+        console.log('User created: ', response)
+    } catch (error) {
+        if (error instanceof Error && error.name === "MongoError" && (error as MongoError).code === 11000)
+            res.status(400).json({ status: 'error', error: 'Username already in use' });
+        else
+            throw error;
+    }
+
+    res.status(200).json({ status: 'ok' });
 });
 
 account.post('/change-password', async (req, res) => {
-  const { token, newpassword: plainTextPassword } = req.body
+    const { token, newpassword: plainTextPassword } = req.body
 
-  if (!plainTextPassword || typeof plainTextPassword !== 'string') {
-      return res.json({ status: 'error', error: 'Invalid password' })
-  }
+    if (!plainTextPassword || typeof plainTextPassword !== 'string') { res.status(400).json({ status: 'error', error: 'Invalid password' }); return; }
 
-  if (plainTextPassword.length <= 5) {
-      return res.json({ status: 'error', error: 'Password too small. Should be atleast 6 characters' })
-  }
+    if (plainTextPassword.length <= 5) { res.json({ status: 'error', error: 'Password too small. Should be atleast 6 characters' }); return; }
 
-
-  try {
-      const user = jwt.verify(token, JWT_SECRET) as { id: string, username: string };
-      const _id = user.id
-      const password = await bcrypt.hash(plainTextPassword, 10)
-      await User.updateOne({ _id }, {
-          $set: { password }
-      })
-      res.json({ status: 'ok' })
-  } catch (error) {
-      res.json({ status: 'error', error: ' :) ' })
-  }
+    try {
+        const user = jwt.verify(token, JWT_SECRET) as { id: string, username: string };
+        const _id = user.id
+        const password = await bcrypt.hash(plainTextPassword, 10)
+        await User.updateOne({ _id }, {
+            $set: { password }
+        })
+        res.status(200).json({ status: 'ok' });
+    } catch (error) {
+        res.status(400).json({ status: 'error', error: ' :) ' });
+    }
 });
 
 export default account;
