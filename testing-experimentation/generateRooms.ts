c1e6e0d953b863e9ba2f5bb19f290eb8052e1217
Tenebrosful <hugobernard39@gmail.com>
2022-01-05 13:07:14 +0100

    Add a testing script to generate rooms

---
+++ testing-experimentation/generateRooms.ts
@@ -0,0 +1,19 @@
+import { Room } from "../src/class/Room";
+import * as random_generator from "random-seed";
+import { randomBytes } from "crypto";
+
+const seed = randomBytes(10).toString("hex");
+// const seed = "oui";
+
+const rand: random_generator.RandomSeed = random_generator.create(seed);
+
+console.log("Seed = ", seed);
+
+const generateRooms: Room[] = [];
+
+for (let i = 0; i < 20; i++)
+  generateRooms.push(Room.generateRandom(rand));
+
+generateRooms.forEach(room => console.log(room.renderTextTiles()));
+
+rand.done();
