7550b8e81435a6fa0e4234de673070e6d6f5fe85
Clément Boulet <clement.boulet60510@gmail.com>
2022-01-06 15:07:25 +0100

    Navbar dynamique selon si l'utilisateur est connecté ou non

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,27 +1,61 @@
 const token = localStorage.getItem("token")
 console.log(token);
 
-const dropdown = document.getElementById("menu-dropdown");
+const dropdown = document.getElementById("menu-dropdown2");
 if(token){
-    dropdown.innerHTML = `   
-        <li><a class="dropdown-item" href="#">Profil</a></li>
-        <li><a class="dropdown-item" href="#">Paramètres</a></li>
-        <hr> 
-        <li><a class="dropdown-item" href="#">Deconnexion</a></li> `
+    const liProfil = document.createElement('li')
+        const aProfil = document.createElement('a')
+            aProfil.classList.add("dropdown-item")
+            aProfil.href="/profil"
+            aProfil.innerHTML="Profil"
+        liProfil.appendChild(aProfil)
+
+    const liParam = document.createElement('li')
+        const aParam = document.createElement('a')
+            aParam.classList.add("dropdown-item")
+            aParam.href="/params"
+            aParam.innerText="Paramètres"
+        liParam.appendChild(aParam)
+
+    const hr = document.createElement('hr')
+
+    const liLogout = document.createElement('li')
+        const aLogout = document.createElement('a')
+            aLogout.classList.add("dropdown-item")
+            aLogout.href="/"
+            aLogout.innerText="Deconnexion"
+            aLogout.addEventListener("click", function(){
+                localStorage.clear()
+                document.location.href = '/';
+            })
+        liLogout.appendChild(aLogout)
+
+    dropdown.appendChild(liProfil)
+    dropdown.appendChild(liParam)
+    dropdown.appendChild(hr)
+    dropdown.appendChild(liLogout)
+
+    
+
 }else{
-    dropdown.innerHTML = `
-        <li><a class="dropdown-item" href="/signup">Inscription</a></li>
-        <hr>
-        <li><a class="dropdown-item" href="/signin">Connexion</a></li>
-       `
+    const liSignup = document.createElement('li')
+        const aSignup = document.createElement('a')
+            aSignup.classList.add("dropdown-item")
+            aSignup.setAttribute("href", "/signup");
+            aSignup.innerHTML="Inscription"
+        liSignup.appendChild(aSignup)
+
+    const hr = document.createElement('hr')
+
+    const liSignin = document.createElement('li')
+        const aSignin = document.createElement('a')
+            aSignin.classList.add("dropdown-item")
+            aSignin.setAttribute("href", "/signin");
+            aSignin.innerText="Connexion"
+        liSignin.appendChild(aSignin)
+
+    dropdown.appendChild(liSignup)
+    dropdown.appendChild(hr)
+    dropdown.appendChild(liSignin)
+
 }
-function oooo(){
-    oui = getElementById("menu-dropdown");
-    const lol = document.createElement('li')
-    lol.classList.add("dropdown-item")
-    lol.innerHTML="deconnexion"
-    lol.href = "/"
-    lol.addEventListener("click", function(){
-        localStorage.clear();
-    });
-    oui.appendChild(lol)}
