b39549a4e3be823a065dc619aac94fa9dba6471a
Tenebrosful <hugobernard39@gmail.com>
2022-01-17 03:35:24 +0100

    Create generateStage.ts

---
+++ testing-experimentation/generateStage.ts
@@ -0,0 +1,22 @@
+import * as random_generator from "random-seed";
+import { randomBytes } from "crypto";
+import { Stage } from "../src/class/Stage";
+
+function run() {
+  const seed = randomBytes(10).toString("hex");
+  // const seed = "oui";
+
+  const rand: random_generator.RandomSeed = random_generator.create(seed);
+
+  console.log("Seed = ", seed);
+
+  for (let i = 1; i <= 4; i++) {
+    console.log(`==== Étage ${i} ====`);
+    new Stage({ floor: i }, rand);
+  }
+
+  rand.done();
+}
+
+run();
+// setInterval(run, 5000);
