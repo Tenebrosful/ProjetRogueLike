b21693460e1ff2ca48df6c5480b0fd29a8309b93
Tenebrosful <hugobernard39@gmail.com>
2022-02-10 09:16:35 +0100

    Update GameRender.ts

--- game/src/class/GameRender.ts
+++ game/src/class/GameRender.ts
@@ -1,3 +1,6 @@
+import Entity from "./entities/Entity";
+import Game from "./Game";
+import Logger from "./Logger";
 import Room from "./Room";
 import Tile from "./tiles/Tile";
 
@@ -5,7 +8,24 @@ export default class GameRender {
   static TILE_SIZE = 64;
   static TILE_ROOT_PATH = "/static/img/tiles/";
 
-  static _salle2D = document.getElementById("salle_2d") as HTMLDivElement;
+  private static _salle2D = document.getElementById("salle_2d") as HTMLDivElement;
+  private static _canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  private static _ctx = GameRender._canvas.getContext("2d") as CanvasRenderingContext2D;
+
+  private static _canvasWidth: number;
+  private static _canvasHeight: number;
+  private static _canvasRatio: number;
+
+  static setupCanvas() {
+    this._canvasWidth = Game.currentRoom.width * this.TILE_SIZE;
+    this._canvasHeight = Game.currentRoom.height * this.TILE_SIZE;
+    this._canvasRatio = window.devicePixelRatio;
+
+    this._canvas.width = this._canvasWidth * this._canvasRatio;
+    this._canvas.height = this._canvasHeight * this._canvasRatio;
+    this._canvas.style.width = `${this._canvasWidth}px`;
+    this._canvas.style.height = `${this._canvasHeight}px`;
+  }
 
   static renderTile(tile: Tile) {
     const renderTile = document.createElement("div");
@@ -22,6 +42,38 @@ export default class GameRender {
   }
 
   static renderRoom(room: Room) {
+    Logger.log(`Render room [${room.coords.posX};${room.coords.posY}]`, "RENDER");
     room.tiles.forEach(line => line.forEach(tile => this.renderTile(tile)));
   }
+
+  static renderCanvas() {
+    Logger.log(`Render Canvas (width: ${this._canvasWidth}, heigh: ${this._canvasHeight})`, "RENDER");
+    this._ctx.clearRect(0, 0, this._canvasWidth, this._canvasHeight);
+  }
+
+  static renderEntity(entity: Entity) {
+    Logger.log(`Render Entity ${typeof entity} at [${entity.coords.posX};${entity.coords.posY}]`, "RENDER");
+
+    const entitySprite = new Image();
+    entitySprite.src = `/static/img/${entity.currentSprite}`;
+
+    this._ctx.drawImage(entitySprite, entity.coords.posX, entity.coords.posY);
+  }
+
+  static renderRoomEntities(room: Room) {
+    Logger.log(`Render all entities of room [${room.coords.posX};${room.coords.posY}]`, "RENDER");
+    room.entities.forEach(entity => this.renderEntity(entity));
+  }
+
+  static renderAll() {
+    this.setupCanvas();
+    this.renderCanvas();
+    this.renderRoom(Game.currentRoom);
+    this.renderAllDynamic();
+  }
+
+  static renderAllDynamic() {
+    this.renderRoomEntities(Game.currentRoom);
+  }
+
 }
