5405e1660d77d2952209b8cf724e49cb53a667ea
Tenebrosful <hugobernard39@gmail.com>
2022-02-10 09:15:51 +0100

    Add log filter

--- game/src/class/Logger.ts
+++ game/src/class/Logger.ts
@@ -1,14 +1,21 @@
 export default abstract class Logger {
   private static _enable = false;
 
+  private static _enableAll = false;
+  private static _enabledLogs: LogType[] = [];
+
   static log(message: any, type: LogType = "OTHER") {
     if (!Logger._enable) return;
+    if (!this._enableAll && !this._enabledLogs.includes(type)) return;
 
-    console.log(`[${type}]\t${message}`);
+      console.log(`[${type}]\t${message}`);
   }
 
-  static enable() {
+  static enable(logsToEnable?: LogType[] | "ALL") {
     Logger._enable = true;
+
+    if (logsToEnable === "ALL") { this._enableAll = true; return; }
+    if (logsToEnable) this._enabledLogs = logsToEnable;
   }
 
   static disable() {
