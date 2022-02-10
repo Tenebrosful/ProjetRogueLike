1260e0134fa199e2805c124e77248bf70ef1a022
Tenebrosful <hugobernard39@gmail.com>
2022-02-10 05:44:10 +0100

    Create Controls.ts

---
+++ game/src/class/Controls.ts
@@ -0,0 +1,48 @@
+export default class Controls {
+  static controls = {
+    walking: {
+      down: "KeyS",
+      left: "KeyQ",
+      right: "KeyD",
+      up: "KeyZ"
+    }
+  };
+
+  static currentState = {
+    walking: {
+      down: false,
+      left: false,
+      right: false,
+      up: false
+    }
+  };
+
+  static async setup() {
+
+    window.onkeyup = (e) => {
+      e.preventDefault();
+
+      if (e.code === this.controls.walking.up)
+        this.currentState.walking.up = true;
+      else if (e.code === this.controls.walking.down)
+        this.currentState.walking.down = true;
+      else if (e.code === this.controls.walking.left)
+        this.currentState.walking.left = true;
+      else if (e.code === this.controls.walking.right)
+        this.currentState.walking.right = true;
+    };
+
+    window.onkeydown = (e) => {
+      e.preventDefault();
+
+      if (e.code === this.controls.walking.up)
+        this.currentState.walking.up = false;
+      else if (e.code === this.controls.walking.down)
+        this.currentState.walking.down = false;
+      else if (e.code === this.controls.walking.left)
+        this.currentState.walking.left = false;
+      else if (e.code === this.controls.walking.right)
+        this.currentState.walking.right = false;
+    };
+  }
+}
