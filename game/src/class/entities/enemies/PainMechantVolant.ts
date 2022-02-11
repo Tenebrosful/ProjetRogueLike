72c016a088b02a987fd6bcbfedf0fb833511f92a
Tenebrosful <hugobernard39@gmail.com>
2022-02-11 04:56:26 +0100

    Create PainMechantVolant.ts

---
+++ game/src/class/entities/enemies/PainMechantVolant.ts
@@ -0,0 +1,32 @@
+import IAMovementFollowPlayer from "../../ia/movement/IAMovementFollowPlayer";
+import ThinkingEntity from "../ThinkingEntity";
+
+export default class PainMechantVolant extends ThinkingEntity {
+  iaMovement = new IAMovementFollowPlayer(5);
+
+  sprites = {
+    idle: "enemies/painMechantVolant/painMechantVolantIdle.png",
+    walking: {
+      down: "enemies/painMechantVolant/painMechantVolantR.png",
+      left: "enemies/painMechantVolant/painMechantVolantL.png",
+      right: "enemies/painMechantVolant/painMechantVolantR.png",
+      up: "enemies/painMechantVolant/painMechantVolantL.png"
+    }
+  };
+
+  hitbox = {
+    offset: {
+      x: 4, // Réduction de 4px pour pas qu'il soit bloqué dans les couloir de 1 case de large
+      y: 0
+    },
+    size: {
+      height: 46,
+      width: 64
+    }
+  };
+
+  canFly = true;
+
+  currentSprite = this.sprites.idle;
+  movementSpeed = 1.5;
+}
