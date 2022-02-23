a1c99c77748e581ac9c7739c9f2ab73675bbf1df
Clément Boulet <clement.boulet60510@gmail.com>
2022-02-23 09:31:37 +0100

    Ajout d'un tableau de parties au modele des users

--- web-server/src/database/models/User.ts
+++ web-server/src/database/models/User.ts
@@ -2,7 +2,12 @@ import * as mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
     password: { required: true, type: String },
-    username: { required: true, type: String, unique: true }
+    username: { required: true, type: String, unique: true },
+    parties : [
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "GameSchema"
+        }]
 }, { collection: "users" });
 
 export const User = mongoose.model("UserSchema", UserSchema);
