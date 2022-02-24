03404c620760ab1b4567be6d358fc4f36f34e91d
Clément Boulet <clement.boulet60510@gmail.com>
2022-02-24 17:36:18 +0100

    Ajout d'un attribut date à la partie

--- web-server/src/database/models/Game.ts
+++ web-server/src/database/models/Game.ts
@@ -1,6 +1,7 @@
 import * as mongoose from "mongoose";
 
 const GameSchema = new mongoose.Schema({
+    gameDate : { required: true, type: String},
     collectedItems : { required: true, type: String },
     coveredStage : { required: true, type: String },
     killedMonster : { required: true, type: String },
