c134b7918dae7cdddb8011c881a421d5d02f8056
Tenebrosful <hugobernard39@gmail.com>
2022-03-06 17:36:47 +0100

    Fix small XL

--- game/src/class/Stage.ts
+++ game/src/class/Stage.ts
@@ -31,7 +31,7 @@ export default class Stage {
 
     this.isXL = random.intBetween(1, 100) <= Stage._chanceToXL;
 
-    this._maxRoomNumber = Math.floor(Stage._baseRoomNumber * (Math.max(this.floor / 5, 1)) * (this.isXL ? random.floatBetween(1.5, 1.5 * (this.floor / 5)) : 1));
+    this._maxRoomNumber = Math.floor(Stage._baseRoomNumber * Math.max(this.floor / 5, 1) * (this.isXL ? random.floatBetween(1.5, 1.5 * Math.max(this.floor / 5, 1)) : 1));
 
     Logger.log(`Nombre de salle maximum : ${this._maxRoomNumber}${this.isXL ? " (XL)" : ""}`, "STAGE");
 
